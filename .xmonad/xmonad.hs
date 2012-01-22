9e427ab0d487fd38ab7c4c5213e377bc7aff61e4
Arne Brasseur <arne.brasseur@gmail.com>
2012-01-22 13:56:47 +0800

    xmonad : border width=1, terminal=lxterminal

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,17 @@
 -- -*- haskell -*-
 
+module Main (main) where
+
 import XMonad
- 
+import XMonad.Util.EZConfig
+
+main :: IO ()
 main = do
     xmonad $ defaultConfig
-     { terminal    = "gnome-terminal --hide-menubar"
+     { terminal    = "lxterminal"
      , modMask = mod4Mask
-     , borderWidth = 3
+     , borderWidth = 1
      }
+     `additionalKeys`
+     [((0, 0x1008ff2d), spawn "sh -c 'xscreensaver-command -l && sudo /usr/sbin/pm-suspend'")
+     ]
