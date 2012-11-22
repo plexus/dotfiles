4d397c65ba5b1e51dbbafa1fceb14109d69b66a0
Arne Brasseur <arne.brasseur@rendaosolutions.com>
2012-11-22 09:58:17 +0100

    Small file name changes to accompany lightdm-session

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,15 +3,25 @@
 module Main (main) where
 
 import XMonad
+import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig
+
+myManageHook = composeAll
+   [ className =? "Firefox"   --> doShift "9"
+   --, className =? "Xmessage"  --> doFloat
+   , manageDocks
+   ]
 
 main :: IO ()
 main = do
     xmonad $ defaultConfig
      { terminal    = "lxterminal"
-     , modMask = mod4Mask
+     , modMask     = mod4Mask
      , borderWidth = 1
+     , workspaces  = ["1:www","2:term","3:emacs","4","5","6","7","8","9","0","-:selenium","=:tmp"]
+     , manageHook  = myManageHook <+> manageHook defaultConfig
      }
      `additionalKeys`
      [((0, 0x1008ff2d), spawn "sh -c 'xscreensaver-command -l && sudo /usr/sbin/pm-suspend'")
      ]
+ 
